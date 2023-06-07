5e9df81eb12e6e372897ccbac4762cab4e8911f9
Bianca Beatrice Eni <b.b.eni@student.tue.nl>
2023-06-07 23:56:45 +0200

    Fixed open port discovery

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 import nmap
 import subprocess
 
+
 class Command:
     def power(status: str, duration: int = 200):
 
@@ -20,6 +21,7 @@ class Command:
 # Client chooses the setting to control from the list
 settings = ["Power", "Color", "Brightness", "Temperature"]
 
+
 def choose_setting():
     print("Available settings:")
     for i, setting in enumerate(settings):
@@ -35,18 +37,18 @@ def choose_setting():
 
 
 def map_setting_to_command(setting):
-        if(setting == 'Power'):
-            power = input('Do you want to turn the light on or off? ')
-            return Command.power(power)
-        elif(setting == 'Color'):
-            print('Choose a value between 0 and 255 for each color \n')
-            r = int(input('red: '))
-            g = int(input('green: '))
-            b = int(input('blue: '))
-            return Command.color(r, g, b)
-        elif(setting == 'Brightness'):
-            bright = int(input('Choose a brightnes level between 1 and 100: '))
-            return Command.brightness(bright)
+    if(setting == 'Power'):
+        power = input('Do you want to turn the light on or off? ')
+        return Command.power(power)
+    elif(setting == 'Color'):
+        print('Choose a value between 0 and 255 for each color \n')
+        r = int(input('red: '))
+        g = int(input('green: '))
+        b = int(input('blue: '))
+        return Command.color(r, g, b)
+    elif(setting == 'Brightness'):
+        bright = int(input('Choose a brightnes level between 1 and 100: '))
+        return Command.brightness(bright)
 
 
 def connect_to_port(ip, port):
@@ -60,20 +62,18 @@ def connect_to_port(ip, port):
     except Exception as e:
         print("An error occurred:", str(e))
 
-    
     more = 'y'
-    
+
     while(more != 'n'):
         command = map_setting_to_command(choose_setting())
         send_command(telnet, command)
         response = receive_response(telnet)
         print("Received response: ", response)
         more = input('Another command? (y/n) ')
-        
+
     # Close the Telnet connection
     telnet.close()
     print("Telnet connection closed")
-
 
 
 def send_command(telnet, command):
@@ -95,30 +95,36 @@ def receive_response(tn):
 
 
 def find_open_port(ip):
-    scanner = nmap.PortScanner()
-    scanner.scan(ip, arguments='-T4 -p-')
+    command = ['nmap', '-p', 'T:10000-65535', '--open', ip]
+    output = subprocess.check_output(command).decode('utf-8')
 
-    for host in scanner.all_hosts():
-        if scanner[host].state() == 'up':
-            for proto in scanner[host].all_tcp():
-                port_list = scanner[host][proto].keys()
-                for port in port_list:
-                    if scanner[host][proto][port]['state'] == 'open':
-                        return int(port)
+    lines = output.split('\n')
+    for line in lines:
+        if line.startswith('Host:') or line.startswith('Nmap scan report'):
+            continue
+        elif 'Ports:' in line:
+            port_line = line.split(':')[1].strip()
+            ports = port_line.split(',')
+            for port in ports:
+                if 'open' in port and 'tcp' in port:
+                    open = int(port.split('/')[0])
 
-    return None
+    return open
+
 
 def run_ipconfig():
     output = subprocess.check_output(['ipconfig']).decode('utf-8')
     print(output)
+
 
 def discover_ips(wifi_ip):
     command = ['nmap', '-sn', wifi_ip[:12]+'*']
     output = subprocess.check_output(command).decode('utf-8')
     print(output + '\n')
 
+
 run_ipconfig()
-wifi_ip=input("What is the Wireless LAN adapter Wifi: IPv4 Address?\n")
+wifi_ip = input("What is the Wireless LAN adapter Wifi: IPv4 Address?\n")
 discover_ips(wifi_ip)
 
 # Define the IP address you want to scan for an open port
